5bb9ba3ccd53bc7759cd0bb06614192d7ecba39a
Abhishek jat <abhishekjat80094@gmail.com>
2025-03-05 16:39:34 +0530

    [Abhishek Jat] Add. Introducing DTO and Model to AddressBook App

---
+++ src/main/java/com/bridgelabz/addressbookapp/model/AddressBookModel.java
@@ -0,0 +1,23 @@
+package com.bridgelabz.addressbookapp.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+// Lombok annotation to generate getters, setters, and constructors
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressBookModel {
+    // Unique ID for the address book entry
+    private int id;
+
+    // Name of the person
+    private String name;
+
+    // Phone number of the person
+    private String phone;
+
+    // Email address of the person
+    private String email;
+}
